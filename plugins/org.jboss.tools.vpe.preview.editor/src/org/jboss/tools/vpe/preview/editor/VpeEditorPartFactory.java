a45ab4871bc9a37965643d51665c13d1b8ff8de6
Konstantin Marmalyukov <kmarmaliykov@exadel.com>
2014-05-05 14:07:15 +0300

    1

--- plugins/org.jboss.tools.vpe.preview.editor/src/org/jboss/tools/vpe/preview/editor/VpeEditorPartFactory.java
+++ plugins/org.jboss.tools.vpe.preview.editor/src/org/jboss/tools/vpe/preview/editor/VpeEditorPartFactory.java
@@ -11,22 +11,19 @@
 package org.jboss.tools.vpe.preview.editor;
 
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.Platform;
 import org.eclipse.ui.part.EditorPart;
 import org.eclipse.wst.sse.ui.StructuredTextEditor;
 import org.jboss.tools.jst.web.ui.internal.editor.bundle.BundleMap;
 import org.jboss.tools.jst.web.ui.internal.editor.editor.IVisualEditor;
 import org.jboss.tools.jst.web.ui.internal.editor.editor.IVisualEditorFactory;
 import org.jboss.tools.vpe.editor.VpeEditorPart;
+import org.jboss.tools.vpe.editor.util.VpePlatformUtil;
 import org.jboss.tools.vpe.xulrunner.browser.XulRunnerBrowser;
 
-public class VpeEditorPartFactory implements IVisualEditorFactory {
-	public static final String LOAD_XULRUNNER_ENGINE = "org.jboss.tools.vpe.engine.xulrunner"; //$NON-NLS-1$
-	private static final String SWT_GTK3 = "SWT_GTK3"; //$NON-NLS-1$
-	
+public class VpeEditorPartFactory implements IVisualEditorFactory {	
 	public IVisualEditor createVisualEditor(final EditorPart multiPageEditor, StructuredTextEditor textEditor, int visualMode, BundleMap bundleMap) {
 		//this property is added in VPE tests to make VPE always opened 
-		if (Boolean.valueOf(System.getProperty(LOAD_XULRUNNER_ENGINE))) {
+		if (VpePlatformUtil.isXulrunnerEnabled()) {
 			return getVpeEditor(multiPageEditor, textEditor, visualMode, bundleMap);
 		}
 		
@@ -34,7 +31,7 @@ public class VpeEditorPartFactory implements IVisualEditorFactory {
 		if (isHtmlFile) {
 			return getPreviewEditor(multiPageEditor, textEditor, visualMode, bundleMap);
 		} else {
-			if (XulRunnerBrowser.isCurrentPlatformOfficiallySupported() && !isGTK3()) {
+			if (XulRunnerBrowser.isCurrentPlatformOfficiallySupported() && !VpePlatformUtil.isGTK3()) {
 				return getVpeEditor(multiPageEditor, textEditor, visualMode, bundleMap);
 			} else {
 				return getPreviewEditor(multiPageEditor, textEditor, visualMode, bundleMap);				
@@ -54,14 +51,5 @@ public class VpeEditorPartFactory implements IVisualEditorFactory {
 		return new VpvEditor2(multiPageEditor, textEditor, visualMode, bundleMap);
 	}
 	
-	private static boolean isGTK3() {
-		if (Platform.WS_GTK.equals(Platform.getWS())) {
-			String gtk3 = System.getProperty(SWT_GTK3);
-			if (gtk3 == null) {
-				gtk3 = System.getenv(SWT_GTK3);
-			}
-			return !"0".equals(gtk3); //$NON-NLS-1$
-		}
-		return false;
-	}
+	
 }
