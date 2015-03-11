690376d9757fa261412267ed290f3c4889d2c184
Konstantin Marmalyukov <kmarmaliykov@exadel.com>
2015-03-11 16:31:22 +0300

    Removing vpe: removing unnecessary stuff used for engine dialog

--- plugins/org.jboss.tools.vpe.preview.core/src/org/jboss/tools/vpe/preview/core/exceptions/Messages.java
+++ plugins/org.jboss.tools.vpe.preview.core/src/org/jboss/tools/vpe/preview/core/exceptions/Messages.java
@@ -6,14 +6,10 @@ public class Messages extends NLS {
 	private static final String BUNDLE_NAME = "org.jboss.tools.vpe.preview.core.exceptions.messages"; //$NON-NLS-1$
 	public static String NO_ENGINE_ERROR;
 	public static String MOZILLA_LOADING_ERROR;
-	public static String MOZILLA_EXPERIMENTAL_SUPPORT;
 	public static String MOZILLA_LOADING_ERROR_LINK_TEXT;
 	public static String MOZILLA_LOADING_ERROR_LINK;
-	public static String GTK3_IS_NOT_SUPPORTED;
-	public static String CURRENT_PLATFORM_IS_NOT_SUPPORTED;
 	public static String CANNOT_SHOW_EXTERNAL_FILE;
 	public static String EDITOR_DOES_NOT_SUPPORT_DOM_MODEL;
-	public static String WEBKIT_NOT_SUPPORTED_PREFERENCES;
 	
 	static {
 		// initialize resource bundle
